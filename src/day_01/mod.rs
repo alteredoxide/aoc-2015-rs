7bd9093f5fac3bd9ab12fe03260f23eb8a70ae8e
alteredoxide <me@alteredoxide.com>
2023-10-13 18:51:52 -0700

    correct type in doc comment

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -1,4 +1,4 @@
-//! Avent Of Code 2015
+//! Advent of Code 2015
 //! 
 //! --- Day 1: Not Quite Lisp ---
 //! Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
