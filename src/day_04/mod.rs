13c8aa9ab5cef24955625442bbfe3e272e0d96d4
alteredoxide <me@alteredoxide.com>
2023-10-17 11:56:17 -0700

    add two parallel versions of part 1

--- src/day_04/mod.rs
+++ src/day_04/mod.rs
@@ -16,8 +16,9 @@
 //!   hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970
 //!   looks like 000006136ef....
 
-use std::path::Path;
+use std::{path::Path, thread::JoinHandle, sync::{Arc, atomic::AtomicIsize}};
 
+use crossbeam_channel::{Receiver, Sender};
 use md5;
 
 
@@ -37,13 +38,92 @@ fn part_1(input: &str) -> usize {
         let combined = format!("{input}{i}");
         let digest = md5::compute(&combined);
         let hex_str = format!("{:x}", digest);
-
         if hex_str.starts_with("00000") {
             return i;
         }
-
         i += 1;
     }
+}
+
+
+fn check(input: String, rx: Receiver<usize>, tx: Sender<usize>) {
+    while let Ok(i) = rx.recv() {
+        let combined = format!("{input}{i}");
+        let digest = md5::compute(&combined);
+        let hex_str = format!("{digest:x}");
+        if hex_str.starts_with("00000") {
+            tx.send(i).expect("this shouldn't fail for the given setup");
+            break
+        }
+    }
+}
+
+
+fn check2(input: String, rx: Receiver<usize>, solution: Arc<AtomicIsize>) {
+    while let Ok(i) = rx.recv() {
+        let combined = format!("{input}{i}");
+        let digest = md5::compute(&combined);
+        let hex_str = format!("{digest:x}");
+        if hex_str.starts_with("00000") {
+            solution.store(i as isize, std::sync::atomic::Ordering::Relaxed);
+            break
+        }
+    }
+}
+
+
+fn part_1_par(input: &str, n_threads: usize) -> Result<usize, String> {
+    let (txi, rxi) = crossbeam_channel::bounded::<usize>(n_threads * 2);
+    let (tx_soln, rx_soln) = crossbeam_channel::bounded::<usize>(0);
+    let handles: Vec<JoinHandle<()>> = (0..n_threads).map(|_| {
+        let rxi = rxi.clone();
+        let tx_soln = tx_soln.clone();
+        let input = input.to_owned();
+        std::thread::spawn(move || check(input, rxi, tx_soln))
+    }).collect();
+    let mut i = 0;
+    let solution: usize;
+    loop {
+        if let Ok(soln) = rx_soln.try_recv() {
+            drop(txi);
+            solution = soln;
+            break
+        }
+        txi.send(i).map_err(|e| e.to_string())?;
+        i += 1;
+    }
+    for handle in handles.into_iter() {
+        handle.join().map_err(|e| format!("{:?}", e))?
+    }
+    Ok(solution)
+}
+
+
+/// NOTE: just trying a different take on the solution compared to `part_1_par()`.
+fn part_1_par2(input: &str, n_threads: usize) -> Result<usize, String> {
+    let (txi, rxi) = crossbeam_channel::bounded::<usize>(n_threads * 2);
+    let solution = Arc::new(AtomicIsize::new(-1));
+    let handles: Vec<JoinHandle<()>> = (0..n_threads).map(|_| {
+        let rxi = rxi.clone();
+        let solution = solution.clone();
+        let input = input.to_owned();
+        std::thread::spawn(move || check2(input, rxi, solution))
+    }).collect();
+    let mut i = 0;
+    let mut solni: isize;
+    loop {
+        solni = solution.load(std::sync::atomic::Ordering::Relaxed);
+        if solni > -1 {
+            drop(txi);
+            break
+        }
+        txi.send(i).map_err(|e| e.to_string())?;
+        i += 1;
+    }
+    for handle in handles.into_iter() {
+        handle.join().map_err(|e| format!("{:?}", e))?
+    }
+    Ok(solni as usize)
 }
 
 
@@ -53,6 +133,26 @@ mod tests {
     fn part_1() {
         let input = super::load_input().unwrap();
         let output = super::part_1(input.trim());
+        assert_eq!(output, 254575);
+    }
+    
+    #[test]
+    fn part_1_par1() {
+        // NOTE: time to run becomes unstable above 16 threads, even when more are
+        // available.
+        let n_threads = 16;
+        let input = super::load_input().unwrap();
+        let output = super::part_1_par(input.trim(), n_threads).unwrap();
+        assert_eq!(output, 254575);
+    }
+    
+    #[test]
+    fn part_1_par2() {
+        // NOTE: time to run becomes unstable above 16 threads, even when more are
+        // available.
+        let n_threads = 16;
+        let input = super::load_input().unwrap();
+        let output = super::part_1_par2(input.trim(), n_threads).unwrap();
         assert_eq!(output, 254575);
     }
 
