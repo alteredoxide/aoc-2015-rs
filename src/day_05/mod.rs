5c78fdebc5c21cf28c4ad7c8764591ec11ef3847
alteredoxide <me@alteredoxide.com>
2023-10-18 12:14:48 -0700

    complete day 5

--- src/day_05/mod.rs
+++ src/day_05/mod.rs
@@ -18,6 +18,8 @@
 //!
 //! How many strings are nice?
 
+// ==== Part One
+
 fn three_vowels(s: &str) -> bool {
     let vowels = "aeiou";
     let mut n: usize = 0;
@@ -72,12 +74,69 @@ fn part_1(input: &[String]) -> usize {
     n_nice
 }
 
+// ==== Part Two
+
+fn has_any_pair_twice(s: &str) -> bool {
+    println!("SEQ: {:?}", &s[0..2]);
+    if s.len() < 4 {
+        return false
+    }
+    let mut pairs: Vec<[char; 2]> = Vec::new();
+    let mut chars = s.chars().peekable();
+    while let Some(c) = chars.next() {
+        if let Some(peek) = chars.peek() {
+            let pair = [c, *peek];
+            if pairs.contains(&pair) {
+                if pairs.last().unwrap() != &pair {
+                    // this prevents counting something like "aaa"
+                    return true
+                }
+            }
+            pairs.push(pair);
+        }
+    }
+    return false
+}
+
+
+fn has_sandwhich(s: &str) -> bool {
+    if s.len() < 3 {
+        return false
+    }
+    let mut chars = s.chars().peekable();
+    let mut current_pair: [char; 2] = [
+        chars.next().unwrap(),
+        chars.next().unwrap()
+    ];
+    while let Some(c) = chars.next() {
+        if &c == &current_pair[0] || chars.peek() == Some(&current_pair[1]) {
+            return true
+        }
+        current_pair.swap(0, 1);
+        current_pair[1] = c;
+    }
+    false
+}
+
+
+fn part_2(input: &[String]) -> usize {
+    let n_nice = input.iter()
+        .fold(0_usize, |n, s| {
+            if has_any_pair_twice(&s) && has_sandwhich(&s) {
+                n + 1
+            } else {
+                n
+            }
+        });
+    n_nice
+}
+
 
 #[cfg(test)]
 mod tests {
     use std::{path::Path, fs::File, io::{BufRead, BufReader}};
 
-    fn load_data() -> std::io::Result<Vec<String>> {
+    fn load_input() -> std::io::Result<Vec<String>> {
         let file_path = Path::new(file!())
             .parent()
             .expect("impossible, every file has a parent")
@@ -93,8 +152,15 @@ mod tests {
 
     #[test]
     fn part_1() {
-        let input = load_data().unwrap();
+        let input = load_input().unwrap();
         let output = super::part_1(&input);
         assert_eq!(output, 258);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = load_input().unwrap();
+        let output = super::part_2(&input);
+        assert_eq!(output, 53);
     }
 }
