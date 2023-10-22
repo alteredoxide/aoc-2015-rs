ec7e9390133362c4b124d0b36f72dfc11ded795b
alteredoxide <me@alteredoxide.com>
2023-10-21 19:49:20 -0700

    complete day 6

--- src/day_06/mod.rs
+++ src/day_06/mod.rs
@@ -1,23 +1,58 @@
 //! --- Day 6: Probably a Fire Hazard ---
-//! Because your neighbors keep defeating you in the holiday house decorating contest year after year, you've decided to deploy one million lights in a 1000x1000 grid.
+//! Because your neighbors keep defeating you in the holiday house decorating contest year
+//! after year, you've decided to deploy one million lights in a 1000x1000 grid.
 //! 
-//! Furthermore, because you've been especially nice this year, Santa has mailed you instructions on how to display the ideal lighting configuration.
+//! Furthermore, because you've been especially nice this year, Santa has mailed you
+//! instructions on how to display the ideal lighting configuration.
 //! 
-//! Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all start turned off.
+//! Lights in your grid are numbered from 0 to 999 in each direction; the lights at each
+//! corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn
+//! on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each
+//! coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate
+//! pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all
+//! start turned off.
 //! 
-//! To defeat your neighbors this year, all you have to do is set up your lights by doing the instructions Santa sent you in order.
+//! To defeat your neighbors this year, all you have to do is set up your lights by doing
+//! the instructions Santa sent you in order.
 //! 
 //! For example:
 //! 
-//! turn on 0,0 through 999,999 would turn on (or leave on) every light.
-//! toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
-//! turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
+//! - turn on 0,0 through 999,999 would turn on (or leave on) every light.
+//! - toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the
+//!   ones that were on, and turning on the ones that were off.
+//! - turn off 499,499 through 500,500 would turn off (or leave off) the middle four
+//!   lights.
+//!
 //! After following the instructions, how many lights are lit?
 //!
+//! --- Part Two ---
+//! You just finish implementing your winning light pattern when you realize you
+//! mistranslated Santa's message from Ancient Nordic Elvish.
+//! 
+//! The light grid you bought actually has individual brightness controls; each light can
+//! have a brightness of zero or more. The lights all start at zero.
+//! 
+//! The phrase turn on actually means that you should increase the brightness of those
+//! lights by 1.
+//! 
+//! The phrase turn off actually means that you should decrease the brightness of those
+//! lights by 1, to a minimum of zero.
+//! 
+//! The phrase toggle actually means that you should increase the brightness of those
+//! lights by 2.
+//! 
+//! What is the total brightness of all lights combined after following Santa's
+//! instructions?
+//! 
+//! For example:
+//! 
+//! - turn on 0,0 through 0,0 would increase the total brightness by 1.
+//! - toggle 0,0 through 999,999 would increase the total brightness by 2000000.
 //!
 //! MY NOTE: I could have used [[bool; 1000]; 1000], but I chose to use a bitset/bitmap
 //! because while the bool array will take an entire 1Mb in memory, the bitmap will only
 //! consume 125Kb. It does, however, make the implementation of this solution much longer.
+
 
 use std::num::ParseIntError;
 
@@ -174,6 +209,26 @@ fn part_1(input: &[Instruction]) -> Result<usize, String> {
 }
 
 
+fn part_2(input: &[Instruction], lights: &mut [usize], n_cols: usize) -> usize {
+    for instruct in input {
+        let ranges = flat_idx_ranges(&instruct.coords, n_cols);
+        for range in ranges {
+            for idx in range {
+                match instruct.action {
+                    Action::Off => match lights[idx] > 0 {
+                        true => lights[idx] -= 1,
+                        false => {},
+                    },
+                    Action::On => lights[idx] += 1,
+                    Action::Toggle => lights[idx] += 2,
+                }
+            }
+        }
+    }
+    lights.iter().sum()
+}
+
+
 #[cfg(test)]
 mod tests {
     use std::{path::Path, fs::File, io::{BufRead, BufReader}};
@@ -259,5 +314,14 @@ mod tests {
         let input = load_input("input.txt").unwrap();
         let count = super::part_1(&input).unwrap();
         assert_eq!(count, 400410);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = load_input("input.txt").unwrap();
+        let mut lights = vec![0_usize; 1_000_000];
+        let n_cols = 1000;
+        let output = super::part_2(&input, &mut lights, n_cols);
+        assert_eq!(output, 26);
     }
 }
