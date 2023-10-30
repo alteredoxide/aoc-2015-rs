4daef8797a824d860dcc75e0af56e0dcdea30822
alteredoxide <me@alteredoxide.com>
2023-10-30 12:30:50 -0700

    complete day 8
    There are two approaches to part 2 for this day.

--- src/day_08/mod.rs
+++ src/day_08/mod.rs
@@ -33,6 +33,25 @@
 //! For example, given the four strings above, the total number of characters of string
 //! code (`2 + 5 + 10 + 6 = 23`) minus the total number of characters in memory for string
 //! values (`0 + 3 + 7 + 1 = 11`) is `23 - 11 = 12`.
+//!
+//! --- Part Two ---
+//! Now, let's go the other way. In addition to finding the number of characters of code,
+//! you should now encode each code representation as a new string and find the number of
+//! characters of the new encoded representation, including the surrounding double quotes.
+//! 
+//! For example:
+//! 
+//! - `""` encodes to `"\"\""`, an increase from `2` characters to `6`.
+//! - `"abc"` encodes to `"\"abc\""`, an increase from `5` characters to `9`.
+//! - `"aaa\"aaa"` encodes to `"\"aaa\\\"aaa\""`, an increase from `10` characters to
+//!   `16`.
+//! - `"\x27"` encodes to `"\"\\x27\""`, an increase from `6` characters to `11`.
+//!
+//! Your task is to find the total number of characters to represent the newly encoded
+//! strings minus the number of characters of code in each original string literal. For
+//! example, for the strings above, the total encoded length (`6 + 9 + 16 + 11 = 42`)
+//! minus the characters in the original code representation (`23`, just like in the first
+//! part of this puzzle) is `42 - 23 = 19`.
 
 fn count_memory_chars(s: &str) -> usize {
     let mut chars = s.trim().chars().peekable();
@@ -62,6 +81,34 @@ fn count_code_chars(s: &str) -> usize {
 }
 
 
+/// This approach would is not only more consise, but it it also more robust: while the
+/// constraints of this problem only expect a couple escape char scenarios, this approach
+/// would also handle other escape chars (e.g. '\t') and patterns.
+fn count_newstr_chars_std(s: &str) -> usize {
+    let escaped = s.escape_default().to_string();
+    let newstr = format!("\"{}\"", escaped);
+    let count = count_code_chars(&newstr);
+    count
+}
+
+
+/// This approach, while not as robust as the one using `escape_default()`, it provides
+/// more insight into what is actually happening.
+fn count_newstr_chars_manual(s: &str) -> usize {
+    let mut newstr = String::from("\"");
+    for c in s.chars() {
+        match c {
+            '"' => newstr.push_str("\\\""),
+            '\\' => newstr.push_str("\\\\"),
+            _ => newstr.push(c),
+        };
+    }
+    newstr.push_str("\"");
+    let count = count_code_chars(&newstr);
+    count
+}
+
+
 fn part_1(input: &[String]) -> usize {
     let mut memory_count: usize = 0;
     let mut code_count: usize = 0;
@@ -70,6 +117,17 @@ fn part_1(input: &[String]) -> usize {
         code_count += count_code_chars(s);
     }
     code_count - memory_count
+}
+
+
+fn part_2(input: &[String]) -> usize {
+    let mut code_count: usize = 0;
+    let mut encoded_count: usize = 0;
+    for s in input {
+        code_count += count_code_chars(s);
+        encoded_count += count_newstr_chars_manual(s);
+    }
+    encoded_count - code_count
 }
 
 
@@ -101,5 +159,18 @@ mod tests {
         let input = load_input("input.txt").unwrap();
         let output = super:: part_1(&input);
         assert_eq!(output, 1333);
+    }
+
+    #[test]
+    fn part_2_sm() {
+        let input = load_input("input_sm.txt").unwrap();
+        let output = super::part_2(&input);
+        assert_eq!(output, 19);
+    }
+    #[test]
+    fn part_2() {
+        let input = load_input("input.txt").unwrap();
+        let output = super::part_2(&input);
+
     }
 }
