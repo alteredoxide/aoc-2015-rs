c614b92199d31a2ac274efc28a53329c3c546eee
alteredoxide <me@alteredoxide.com>
2023-11-01 19:53:43 -0700

    complete day 12

--- src/day_12/mod.rs
+++ src/day_12/mod.rs
@@ -17,20 +17,34 @@
 //! You will not encounter any strings containing numbers.
 //! 
 //! What is the sum of all numbers in the document?
+//! --- Part Two ---
+//! Uh oh - the Accounting-Elves have realized that they double-counted everything red.
+//! 
+//! Ignore any object (and all of its children) which has any property with the value
+//! `"red"`. Do this only for objects (`{...}`), not arrays (`[...]`).
+//! 
+//! - `[1,2,3]` still has a sum of `6`.
+//! - `[1,{"c":"red","b":2},3]` now has a sum of `4`, because the middle object is
+//!   ignored.
+//! - `{"d":"red","e":[1,2,3,4],"f":5}` now has a sum of `0`, because the entire structure
+//!   is ignored.
+//! - `[1,"red",5]` has a sum of `6`, because `"red"` in an array has no effect.
 
 use std::num::ParseIntError;
 
+use serde_json::{self, Value, Number, Map};
 
-fn update_sum(sum: &mut i32, digit: &mut String) -> Result<(), ParseIntError> {
+
+fn update_sum(sum: &mut i64, digit: &mut String) -> Result<(), ParseIntError> {
     if !digit.is_empty() {
-        *sum += digit.parse::<i32>()?;
+        *sum += digit.parse::<i64>()?;
         digit.clear();
     }
     Ok(())
 }
 
 
-fn part_1(input: &str) -> Result<i32, ParseIntError> {
+fn part_1(input: &str) -> Result<i64, ParseIntError> {
     let mut chars = input.chars().peekable();
     let mut sum = 0;
     let mut digit = String::from("");
@@ -50,6 +64,60 @@ fn part_1(input: &str) -> Result<i32, ParseIntError> {
         }
     }
     Ok(sum)
+}
+
+
+fn count_number(x: Number) -> Option<i64> {
+    x.as_i64()
+}
+
+
+fn count_vec(data: Vec<Value>) -> i64 {
+    let mut count: i64 = 0;
+    for val in data {
+        match val {
+            Value::Number(x) => count += count_number(x).expect("no!"),
+            Value::Array(v) => count += count_vec(v),
+            Value::Object(m) => count += count_map(m),
+            _ => {}
+        }
+    }
+    count
+}
+
+
+fn count_map(data: Map<String, Value>) -> i64 {
+    let mut count: i64 = 0;
+    for (_, v) in data {
+        match v {
+            Value::Array(v) => count += count_vec(v),
+            Value::Number(x) => count += count_number(x).expect("no!"),
+            Value::String(s) => if s == "red" { return 0 } else { continue },
+            Value::Object(m) => count += count_map(m),
+            _ => {}
+        }
+    }
+    count
+}
+
+
+fn part_2(input: &str) -> Result<i64, String> {
+    let decoded = serde_json::from_str(input).map_err(|e| e.to_string())?;
+    let data = match decoded {
+        Value::Array(v) => v,
+        Value::Null => return Err(String::from("no data")),
+        _ => return Err(String::from("expected an array for input data"))
+    };
+    let mut count: i64 = 0;
+    for datum in data {
+        match datum {
+            Value::Array(v) => count += count_vec(v),
+            Value::Number(x) => count += count_number(x).expect("no!"),
+            Value::Object(m) => count += count_map(m),
+            _ => {},
+        }
+    }
+    Ok(count)
 }
 
 
@@ -77,6 +145,20 @@ mod tests {
         let input = load_input("input.txt").unwrap();
         let output = super::part_1(&input).unwrap();
         assert_eq!(output, 191164);
+    }
+
+    #[test]
+    fn part_2_sm() {
+        let input = load_input("input_sm_red.txt").unwrap();
+        let output = super::part_2(&input).unwrap();
+        assert_eq!(output, 35);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = load_input("input.txt").unwrap();
+        let output = super::part_2(&input).unwrap();
+        assert_eq!(output, 87842);
     }
 
 }
